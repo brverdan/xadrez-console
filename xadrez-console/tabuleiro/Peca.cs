99154c2519328015c9340600a6027bee910b24aa
Bruno Verdan <bruno.verdan@al.infnet.edu.br>
2020-06-01 18:24:57 -0300

    Comecando a implementar o tabuleiro

---
+++ xadrez-console/tabuleiro/Peca.cs
@@ -0,0 +1,22 @@
+﻿using System.Security.Principal;
+
+namespace tabuleiro
+{
+    class Peca
+    {
+        public Posicao Posicao { get; set; }
+        public Cor Cor { get; set; }
+        public int QteMovimentos { get; set; }
+        public Tabuleiro Tab { get; set; }
+
+        public Peca(Posicao posicao, Tabuleiro tab, Cor cor)
+        {
+            Posicao = posicao;
+            Tab = tab;
+            Cor = cor;
+            QteMovimentos = 0;
+        }
+
+
+    }
+}
